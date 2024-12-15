f8244107e9f3a424e08789674d80c986a67df7aa
mikealynch <mikelynch02115@gmail.com>
2024-12-15 13:10:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -85,8 +85,7 @@ if st.button("Generate Design"):
     else:
         # Enhanced prompt to emphasize the fabric color
         prompt = (    
-            f"A {fabric_color_name}  made of {fabric_type} with {thread_type} thread. "
-            f"The {fabric_color_name} {clothing_type} should be worn by a woman and should be modest and elegant. {fabric_color_name} should be the primary color on the {clothing_type}. "
+            f"Generate a {fabric_color_name} made of {fabric_type}. The {fabric_color_name} {clothing_type} should be worn by a woman and the image should look like it's from a high quality fashion magazine photo shoot. {fabric_color_name} should be the primary color on the {clothing_type}. "
             f"The image should be child-friendly and suitable for a 9-year-old."
             f"No nudity, no violence, no suggestive themes."
         )
